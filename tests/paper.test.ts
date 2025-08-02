ef6cdc2a6fc03e73868fd5251133225413736283
anass-lahlal <anass.lahlal@gmail.com>
2025-08-02 23:28:32 +0100

    test: add failing test for paper writing text

--- tests/paper.test.ts
+++ tests/paper.test.ts
@@ -3,11 +3,20 @@ import {Paper}  from './../src/paper';
 
 
 describe("Paper", ()=>{
+    let paper: Paper;
+
+    beforeEach(() => {
+        paper = new Paper();
+    })
 
     it("is created empty", ()=>{
-        const paper = new Paper();
         expect(paper.content.length).toBe(0);
     });
+
+    it("needs to write text to the content", () => {
+        paper.write("Text");
+        expect(paper.content).toBe("Text");
+    })
 
     
 })
