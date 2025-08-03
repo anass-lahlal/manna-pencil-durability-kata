42a096c72a011a9bc6a9c37348d0b76e94958c13
anass-lahlal <anass.lahlal@gmail.com>
2025-08-03 16:52:04 +0100

    test: add failing test for pencil writing empty space

--- tests/pencil.test.ts
+++ tests/pencil.test.ts
@@ -1,16 +1,24 @@
-
 import { Pencil } from './../src/pencil';
 
 describe("Pencil", () => {
-    it("should initialize values correctly", () => {
-        const pointDurability = 1000;
-        const length = 4;
-        const eraserDurability = 400;
-        const pencil = new Pencil(pointDurability, eraserDurability, length);
+    let pencil: Pencil;
+    const pointDurability = 1000;
+    const length = 4;
+    const eraserDurability = 400;
 
+    beforeEach(() => {
+        pencil = new Pencil(pointDurability, eraserDurability, length);
+    })
+
+    it("should initialize values correctly", () => {
         expect(pencil.pointDurability).toBe(pointDurability);
         expect(pencil.length).toBe(length);
         expect(pencil.eraserDurability).toBe(eraserDurability);
+        expect(pencil.currentPointDurability).toBe(pointDurability);
+    })
+
+    it("should consume 0 point durability on writing space", () => {
+        pencil.write(" ".repeat(10));
         expect(pencil.currentPointDurability).toBe(pointDurability);
     })
 })
