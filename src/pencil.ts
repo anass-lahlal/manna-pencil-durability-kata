dcf0ff20b8cf21d13f0669ff68c682a18516f058
anass-lahlal <anass.lahlal@gmail.com>
2025-08-03 17:30:00 +0100

    feat: consume 0 point on space charachters

--- src/pencil.ts
+++ src/pencil.ts
@@ -10,4 +10,19 @@ export class Pencil {
         this.length = length;
         this.currentPointDurability = pointDurability;
     }
+
+
+    write(text: string): string {
+        let result = "";
+        for(let char of text) {
+            if(/\s/.test(char)) {
+                //do not consume point
+            } else {
+                this.currentPointDurability -= 1
+            }
+            result += char;
+        }
+
+        return result;
+    }
 }
